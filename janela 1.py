bc428500d143489b6758d16648c3f117268a84c7
arthur <arthurfavieira@gmail.com>
2022-02-20 12:19:57 +0000

    erro linha33

--- janela 1.py
+++ janela 1.py
@@ -1,30 +1,7 @@
-import PySimpleGUI as sg
+import random
 
+def gera():
+    return random.randint(1, 10)
 
-#criação primeira janela
-def make_window1():
-    layout = [[ sg.Text("\nPalpite gerado!")],
-              [ sg.Text("Qual seu chute: ",key='-OUTPUT-')],
-              [sg.Input(key='chute')],
-              [sg.Button('PROXIMO'), sg.Button('Fechar')]]
-
-    return sg.Window('Jogo Da Sorte ', layout, )
-
-
-#criando um loop de leitura do evento
-def main():
-    nome = make_window1(), None
-    while True:#loop
-       
-        window, event, values = sg.read_all_windows()
-         #serve para fechar a primeira janela caso aperte fechar
-        if window == nome and event in (sg.WIN_CLOSED, 'Fechar'):
-            break
-        #proxima janela
-        if event == 'INICIAR':
-        # Atualize o elemento de texto "output" para ser o valor do elemento "input"
-            window['-OUTPUT-'].update(values['chute'])#saida de valares 
-    
-
-window.main()
-   
+resposta=gera()
+print(resposta)
--- jogo.demo.py
+++ jogo.demo.py
@@ -21,20 +21,24 @@ def tela_jogo(nome, acertou, resposta, tentativa):
     ]
     window = sg.Window('Jogo Da Sorte ', layout)
 
+
+def gera():
+    return random.randint(1, 10)
+    # cria um valor aleatorio
+
+resposta= gera()
+
 # criar um loop de leitura de eventos
 while True:
-    event, values = window.read()
+    event, values = tela_jogo(chute, acertou, resposta, tentativa)
     # Pega o valor do input de key=chute e transforma em número pq ela vem como string
     chute = int(values['chute'])
     # chama a função game e passa as variáveis para ela, o retorno dela preciso pegar 2 variáveis: resposta e tentativa
-    acertou, tentativa = game(chute, acertou, resposta, tentativa)
+    acertou, tentativa = (chute, acertou, resposta, tentativa)
     # Enquanto a resposta for False vai pedindo o próximo palpite
     if event == sg.WIN_CLOSED or acertou:
         break
 
-def gera():
-    return random.randint(1, 10)
-# cria um valor aleatorio
 
 def game(chute, acertou, resposta, tentativa):
     tentativa += 1 # toda vez que ele tentar adiciona 1
@@ -48,15 +52,14 @@ def game(chute, acertou, resposta, tentativa):
         acertou = True
     return acertou, tentativa
             
-        
-if 'main' == name:
+nome = tela_inicial()
+                    # Inicializa algumas variáveis        
+if 'main' == nome:
             # Retorna o nome escolhido para ser usado na próx. tela
-    nome = tela_inicial()
-                    # Inicializa algumas variáveis
     tentativa = 0
     acertou = False
                     # Gera um número aleatório só uma vez
-    esposta = gera()
+    resposta = gera()
                     # Inicializa o jogo
 
 tela_jogo(nome, acertou, resposta, tentativa)
